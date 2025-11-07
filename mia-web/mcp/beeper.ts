b530907ae9cd30238d5b3ecebe0c5a788820ca25
Alexis Vincent <mail@alexisvincent.io>
2025-11-07 12:14:16 -0800

    With tailscale url

--- mia-web/mcp/beeper.ts
+++ mia-web/mcp/beeper.ts
@@ -1,12 +1,12 @@
 import BeeperDesktop from '@beeper/desktop-api';
 
 const client = new BeeperDesktop({
-  accessToken: process.env.BEEPER_ACCESS_TOKEN
+  accessToken: process.env.BEEPER_ACCESS_TOKEN,
+  baseURL: "https://apollo.emperor-banfish.ts.net/"
 });
 
 
 export async function latest() {
-
   const page = await client.chats.search({ includeMuted: true, limit: 3, type: 'single' });
   const chat = page.items[0];
 
